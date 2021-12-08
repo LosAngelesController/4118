7ce7e299a5919c04b4d946c66e20d46a1ffbf709
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-12-08 12:06:02 -0800

    upload new combined files

---
+++ combining-json/combine.js
@@ -0,0 +1,29 @@
+const sean1 = require('./inputs/sean-dec-8-4118.json');
+const kyler1 = require('./inputs/kyler-dec-8-4118.json');
+const kyleroriginal = require('./inputs/features.json');
+const editJsonFile = require("edit-json-file");
+var fs = require('fs');
+// If the file doesn't exist, the content will be an empty object by default.
+let file = editJsonFile(`${__dirname}/features.json`);
+
+var arrayOfFiles = [sean1,kyler1,kyleroriginal]
+
+var locations =  {
+    features:  [],
+    type: "FeatureCollection"
+  }
+  
+arrayOfFiles.forEach((eachFile, itemIndex) => {
+    console.log(itemIndex + " has " + eachFile.features.length + " locs")
+    locations.features = [...locations.features, ...eachFile.features]
+})
+
+
+console.log(locations.features.length)
+
+var jsonData = JSON.stringify(locations);
+fs.writeFile("features.json", jsonData, function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
