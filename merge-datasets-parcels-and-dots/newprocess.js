4dac56878366b631462e2261a82f3b7e69ecac9b
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2022-07-07 10:13:52 -0700

    okay just push

--- merge-datasets-parcels-and-dots/newprocess.js
+++ merge-datasets-parcels-and-dots/newprocess.js
@@ -15,22 +15,51 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 exports.__esModule = true;
 var joined_fixed_crs_new_school_map_parcels_json_1 = __importDefault(require("./joined-fixed-crs-new-school-map-parcels.json"));
-var cleanedareafeatures = joined_fixed_crs_new_school_map_parcels_json_1["default"].features.map(function (eachFeature) {
-    return {
-        "type": "feature",
-        "properties": {
-            "address": eachFeature.properties['StreetAbr'],
-            "place_name": eachFeature.properties['School'],
-            "set": 10,
-            "section": 1,
-            "date": '7/27/2022',
-            "category": "School",
-            "autoadd": true
-        },
-        "geometry": eachFeature.geometry
-    };
+if (false) {
+    var cleanedareafeatures = joined_fixed_crs_new_school_map_parcels_json_1["default"].features.map(function (eachFeature) {
+        if (eachFeature.geometry.coordinates.length > 1) {
+            console.log('multi');
+        }
+        return {
+            "type": "feature",
+            "properties": {
+                "address": eachFeature.properties['StreetAbr'],
+                "place_name": eachFeature.properties['School'],
+                "set": 10,
+                "section": 1,
+                "date": '7/27/2022',
+                "category": "School",
+                "autoadd": true
+            },
+            "geometry": eachFeature.geometry
+        };
+    });
+}
+var cleanedareafeatures = [];
+joined_fixed_crs_new_school_map_parcels_json_1["default"].features.forEach(function (eachFeature) {
+    if (eachFeature.geometry.coordinates.length > 1) {
+        console.log('multi');
+    }
+    eachFeature.geometry.coordinates.forEach(function (eachCoordSystem) {
+        cleanedareafeatures.push({
+            "type": "feature",
+            "properties": {
+                "address": eachFeature.properties['StreetAbr'],
+                "place_name": eachFeature.properties['School'],
+                "set": 10,
+                "section": 1,
+                "date": '7/27/2022',
+                "category": "School",
+                "autoadd": true
+            },
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": eachCoordSystem
+            }
+        });
+    });
 });
 var writeout = __assign(__assign({}, joined_fixed_crs_new_school_map_parcels_json_1["default"]), { features: cleanedareafeatures });
 var fs = require('fs');
 // Or
-fs.writeFileSync('./output4118', JSON.stringify(writeout));
+fs.writeFileSync('./output4118-autoschools-v2.json', JSON.stringify(writeout));
--- merge-datasets-parcels-and-dots/newprocess.ts
+++ merge-datasets-parcels-and-dots/newprocess.ts
@@ -1,6 +1,14 @@
 import areas from './joined-fixed-crs-new-school-map-parcels.json'
-
+ if (false ) {
+    
 var cleanedareafeatures = areas.features.map((eachFeature) => {
+
+    if (eachFeature.geometry.coordinates.length > 1) {
+        console.log(
+            'multi'
+        )
+    }
+
     return {
         "type": "feature",
         "properties": {
@@ -15,10 +23,44 @@ var cleanedareafeatures = areas.features.map((eachFeature) => {
         "geometry": eachFeature.geometry
     };
 });
+ }
+
+ var cleanedareafeatures = []
+
+ areas.features.forEach((eachFeature) => {
+
+    if (eachFeature.geometry.coordinates.length > 1) {
+        console.log(
+            'multi'
+        )
+    }
+
+    eachFeature.geometry.coordinates.forEach((eachCoordSystem) => {
+        cleanedareafeatures.push({
+            "type": "feature",
+            "properties": {
+                "address": eachFeature.properties['StreetAbr'],
+                "place_name": eachFeature.properties['School'],
+                "set": 10,
+                "section": 1,
+                "date": '7/27/2022',
+                "category": "School",
+                "autoadd": true
+            },
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": eachCoordSystem
+            }
+        })
+    })
+
+  
+});
+ 
 
 var writeout = {...areas, features: cleanedareafeatures}
 
 const fs = require('fs');
 
 // Or
-fs.writeFileSync('./output4118', JSON.stringify(writeout));
+fs.writeFileSync('./output4118-autoschools-v2.json', JSON.stringify(writeout));
