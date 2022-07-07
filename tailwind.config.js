a8a25b7e31d3b266656f5e4b37a55d938c4f5e1d
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2022-07-07 09:58:51 -0700

    webpack files

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+   // maxWidth: {
+      //'xxs': '18rem'
+    //},
+    extend: {
+      maxWidth: {
+        'xxs': '18rem'
+      },
+      colors: {
+        truegray: colors.trueGray,
+        mejito: '#41ffca'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('tailwind-scrollbar')],
+}
